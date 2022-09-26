8a2114a3ce766173c7c9ab3c71d18018cebb25e3
sfjbssept <sfjbssept@gmail.com>
2022-09-26 14:52:55 +0000

    mongo

--- Node-Rest-Api-Mongo/routes/courses.js
+++ Node-Rest-Api-Mongo/routes/courses.js
@@ -31,12 +31,12 @@
 //         const post = req.body;
 //         console.log(post)
 //         const newpost = new Course(post)
-        
+
 //         //Course.create(new)
 //         const course = await newpost.save();
 //         if(!course)
 //         res.status(201).send(course);
-        
+
 //     } catch (err) {
 //        // res.json(err);
 //         console.log(err.message)
@@ -54,37 +54,50 @@
 
 // module.exports = router;
 const ex = require("express");
+const course = require("../models/course");
 const Course = require("../models/course");
 const router = ex.Router();
 
 // get all courses
 router.get("/allcourses", async (req, res) => {
-  try {
-    const courses = await Course.find();
-    res.json(courses);
-  } catch(err) {
-    res.json(err);
-  }
-});
-
-// add a course
-
- router.post("/addcourse",async(req,res) => {
-    try{
-      const course= await Course.create(req.body);
-      res.json(course);
-    } catch (err) {
-        res.json(err)
-    }
- })
-
- router.delete("/delete/:courseId", async(req,res)=>{
-    try{
-       const result= await Course.remove({_id:req.params.courseId});
-       res.status(200).json({message:"deleted successfully"})
+    try {
+        const courses = await Course.find();
+        res.json(courses);
     } catch (err) {
         res.json(err);
     }
 });
+
+// add a course
+
+router.post("/addcourse", async (req, res) => {
+    try {
+        const course = await Course.create(req.body);
+        res.json(course);
+    } catch (err) {
+        res.json(err)
+    }
+})
+
+router.delete("/delete/:courseId", async (req, res) => {
+    try {
+        const result = await Course.remove({ _id: req.params.courseId });
+        res.status(200).json({ message: "deleted successfully" })
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.put("/update/:courseId", async (req, res) => {
+    const courseId = req.params.courseId;
+    try {
+        const course = await Course.updateOne({
+            "_id": courseId
+        }, req.body);
+        res.json(course);
+    } catch (error) { 
+        res.json(error);
+    }
+})
 
 module.exports = router;
