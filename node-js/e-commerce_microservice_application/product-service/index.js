fc3f7e84cef10995fe571803b3f17dee2f30bf22
sfjbssept <sfjbssept@gmail.com>
2022-10-04 17:19:01 +0000

    updated index.js

--- node-js/e-commerce_microservice_application/product-service/index.js
+++ node-js/e-commerce_microservice_application/product-service/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 5001;
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
