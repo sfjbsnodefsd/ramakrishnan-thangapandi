82181560f661de9b4044d342b57de2af5d770af9
Ramakrishnan thangapandi <ramakrishnan.thangapandi@tcs.com>
2022-10-20 12:29:51 +0000

    Added producer

--- node-js/Kafka-demo-app/producer/index.js
+++ node-js/Kafka-demo-app/producer/index.js
@@ -1,1 +1,17 @@
-console.log("producer...");
+console.log("producer");
+const Kafka = require("node-rdkafka");
+
+const stream = Kafka.createWriteStream(
+    {
+        "metadata.broker.list": "localhost:9092",
+    },
+    {},
+    { topic: "test" }
+);
+function queueMessage() {
+    const result = stream.write(Buffer.from("This is rama"))
+    console.log(result);
+}
+setInterval(() => {
+    queueMessage();
+}, 3000)
