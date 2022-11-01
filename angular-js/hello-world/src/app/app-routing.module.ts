cc1ad0680edbec4da59daa9d1047a26eba23a7db
Ramakrishnan thangapandi <ramakrishnan.thangapandi@tcs.com>
2022-11-01 10:57:12 +0000

    Pipe demo coding

--- angular-js/hello-world/src/app/app-routing.module.ts
+++ angular-js/hello-world/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserFormComponent } from './user-form/user-form.component';
-import { HomeComponent } from './home/home.component';
-import { RegisteredUsersComponent } from './registered-users/registered-users.component';
-import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
+import { UserFormComponent } from './Components/user-form/user-form.component';
+import { HomeComponent } from './Components/home/home.component';
+import { RegisteredUsersComponent } from './Components/registered-users/registered-users.component';
+import { PipeDemoComponent } from './Components/pipe-demo/pipe-demo.component';
 
 const routes: Routes = [{path:"",component:HomeComponent},
 {path:"register",component:UserFormComponent}
