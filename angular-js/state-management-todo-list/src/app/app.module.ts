d85816b41f7f422cbaf146a95604ab7380fac4ad
Ramakrishnan thangapandi <ramakrishnan.thangapandi@tcs.com>
2022-11-02 07:42:03 +0000

    Added add-todo and display-todo

--- angular-js/state-management-todo-list/src/app/app.module.ts
+++ angular-js/state-management-todo-list/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoReducer } from './reducers/todo.reducer';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { DisplayTodoComponent } from './components/display-todo/display-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    DisplayTodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-js/state-management-todo-list/src/app/components/display-todo/display-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-todo',
+  templateUrl: './display-todo.component.html',
+  styleUrls: ['./display-todo.component.css']
+})
+export class DisplayTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
